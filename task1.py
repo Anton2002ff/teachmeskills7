0211e75b652c95959f1dc4596d29ad8dcde40800
Anton2002ff <anton.poloneichik232@gmail.com>
2024-12-01 18:39:59 +0300

    Lambda

---
+++ task1.py
@@ -0,0 +1,12 @@
+#Задание 1 по Lambda
+
+# greet = lambda name: f"Hello, {name}"
+# print(greet("Anton"))
+
+
+#Задание 2
+
+names = ["Егор", "Влад", "Кристина"]
+formatted_names = list(map(lambda name: f"Hello, {name}", names))
+print(formatted_names)
+
